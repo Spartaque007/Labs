51033391c6fa893f9ce140a37054ca50cbc60328
Spartaque <yauheni.hlinski@itechart-group.com>
2019-08-01 17:57:22 +0300

    Keyboard Spy

---
+++ KeyboardSpy/KeyboardSpy/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace KeyboardSpy
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            MessageBox.Show("dsfdfd");
+            Keyboard a = new Keyboard();
+            Thread g = new Thread(() =>
+            {
+                    a.SetHooks();
+            });
+            g.Start();
+
+            while(true)
+            {
+
+            }
+            a.UnHook();
+
+        }
+
+    }
+}
