1be5ae74834e70e866b470cf450a6336323129e4
Spartaque <yauheni.hlinski@itechart-group.com>
2019-08-06 16:11:53 +0300

    Delegate

---
+++ Exercises.Delegate/Exercises.Delegate/Point.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+
+namespace Exercises.Delegate
+{
+    public class Point : IComparable
+    {
+
+        public enum PointRepresentation { Polar, Rectangular }
+
+        private double r, a;
+
+        public Point(double x, double y)
+        {
+            r = RadiusGivenXy(x, y);
+            a = AngleGivenXy(x, y);
+        }
+
+        public Point(double par1, double par2, PointRepresentation pr)
+        {
+            if (pr == PointRepresentation.Polar)
+            {
+                r = par1; a = par2;
+            }
+            else
+            {
+                r = RadiusGivenXy(par1, par2);
+                a = AngleGivenXy(par1, par2);
+            }
+        }
+
+        public double X
+        {
+            get { return XGivenRadiusAngle(r, a); }
+        }
+
+        public double Y
+        {
+            get { return YGivenRadiusAngle(r, a); }
+        }
+
+
+        public double Radius
+        {
+            get { return r; }
+        }
+
+        public double Angle
+        {
+            get { return a; }
+        }
+
+        public void Move(double dx, double dy)
+        {
+            double x, y;
+            x = XGivenRadiusAngle(r, a); y = YGivenRadiusAngle(r, a);
+            r = RadiusGivenXy(x + dx, y + dy);
+            a = AngleGivenXy(x + dx, y + dy);
+        }
+
+        public void Rotate(double angle)
+        {
+            a += angle;
+        }
+
+        public override string ToString()
+        {
+            return "(" + XGivenRadiusAngle(r, a) + "," + YGivenRadiusAngle(r, a) + ")";
+        }
+
+
+        private static double RadiusGivenXy(double x, double y)
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+
+        private static double AngleGivenXy(double x, double y)
+        {
+            return Math.Atan2(y, x);
+        }
+
+        private static double XGivenRadiusAngle(double r, double a)
+        {
+            return r * Math.Cos(a);
+        }
+
+        private static double YGivenRadiusAngle(double r, double a)
+        {
+            return r * Math.Sin(a);
+        }
+
+        
+        public int CompareTo(object obj)
+        {
+            Point x = obj as Point;
+            if (x != null )
+            {
+                return this.r.CompareTo(x.r);
+            }
+            else
+            {
+                throw new ArgumentException(" Parameters type filed");
+            }
+        }
+    }
+
+}
---
+++ Exercises.Delegate/Exercises.Delegate/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Exercises.Delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point[] points = new Point[10];
+            ArrayIni(points);
+            Point b = Array.Find(points, GetMinPoint);
+            Console.WriteLine($"First min point: {b}");
+            Console.WriteLine();
+            PrintArray<Point>(points);
+            Array.Sort(points, Compare);
+            PrintArray<Point>(points);
+            ArrayIni(points);
+            PrintArray<Point>(points);
+            Array.Sort(points);
+            PrintArray<Point>(points);
+            Console.ReadLine();
+        }
+
+        static void ArrayIni(Point[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Point(array.Length-i, array.Length - i);
+            }
+        }
+
+        static bool GetMinPoint(Point point)
+        {
+            return Math.Sqrt(Math.Pow(point.X, 2) + Math.Pow(point.Y, 2)) < 1;
+
+
+        }
+
+        static void PrintArray<T>(T[] array)
+        {
+            Console.WriteLine("Array :");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+            }
+            Console.WriteLine();
+        }
+
+        static int Compare(Point a, Point b)
+        {
+            return (a.X + a.Y).CompareTo(b.X + b.Y);
+        }
+
+    }
+}
