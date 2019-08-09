ac674f151e882451b9de13d573c3536953d0fb54
Yauheni.Hlinski <yauheni.hlinski@itechart-group.com>
2019-08-09 17:39:51 +0300

    Add model and parsing url

---
+++ Exercises.String/Exercises.String/GoogleMapsModel.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Web;
+
+namespace Exercises.String
+{
+    class GoogleMapsModel
+    {
+        private readonly char[] _separators = { '&' };
+
+        public string UrlPath { get; set; }
+
+        public string Origin { get; set; }
+
+        public string Destination { get; set; }
+
+        public string TravelMode { get; set; }
+
+        public string Waypoint_place_id { get; set; }
+
+        public List<string> Waypoints { get; set; } = new List<string>();
+
+
+        public GoogleMapsModel(string url)
+        {
+            var urlArray = GetArrayFromUrl(url);
+            UrlPath = urlArray[0];
+            for (int i = 1; i < urlArray.Length; i++)
+            {
+                if (urlArray[i].Contains("origin"))
+                {
+                    Origin = urlArray[i].Substring(7);
+                }
+                else if (urlArray[i].Contains("destination"))
+                {
+                    Destination = urlArray[i].Substring(12);
+                }
+                else if (urlArray[i].Contains("waypoints"))
+                {
+                    char[] separators = { '|', '=' };
+                    string[] _waypoints = urlArray[i].Split(separators);
+                    for (int j = 1; j < _waypoints.Length; j++)
+                    {
+                        Waypoints.Add(_waypoints[j]);
+                    }
+                }
+                else if (urlArray[i].Contains("travelmode"))
+                {
+                    TravelMode = urlArray[i].Substring(11);
+                }
+                else if (urlArray[i].Contains("waypoint_place_ids"))
+                {
+                    Waypoint_place_id = urlArray[i].Substring(19);
+                }
+            }
+        }
+
+
+        private string[] GetArrayFromUrl(string url)
+        {
+            string decodingUrl = HttpUtility.UrlDecode(url);
+            string[] urlArray = decodingUrl.Split(_separators);
+            return urlArray;
+        }
+    }
+}
--- Exercises.String/Exercises.String/Program.cs
+++ Exercises.String/Exercises.String/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Exercises.String
 {
@@ -10,6 +6,19 @@ namespace Exercises.String
     {
         static void Main(string[] args)
         {
+            string url = "https://www.google.com/maps/dir/" +
+                "?api=1&" +
+                "origin=Paris,France&" +
+                "destination=Cherbourg,France&" +
+                "travelmode=driving&" +
+                "waypoints=Versailles,France%7" +
+                "CChartres,France%7" +
+                "CLe+Mans,France%7" +
+                "CCaen,France&" +
+                "waypoint_place_ids=ChIJdUyx15R95kcRj85ZX8H8OAU%7CChIJKzGHdEgM5EcR_OBTT3nQoEA%7CChIJG2LvQNCI4kcRKXNoAsPi1Mc%7CChIJ06tnGbxCCkgRsfNjEQMwUsc";
+
+            GoogleMapsModel travelWay = new GoogleMapsModel(url);
+            Console.Read();
         }
     }
 }
