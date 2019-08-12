6ba75978153e3890c3fc5473f0351318f91d993a
Yauheni.Hlinski <yauheni.hlinski@itechart-group.com>
2019-08-12 09:49:19 +0300

    All Work

---
+++ KeyboardSpy/KeyboardSpy/KeyboardSpy.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+
+namespace KeyboardSpy
+{
+    public static class KeyboardSpy
+    {
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static Action<int> _action;
+
+
+        public static void SetAction(Action<int> action)
+        {
+            _action = action;
+        }
+
+        public static IntPtr SetHook()
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, _proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        public static void Unhook()
+        {
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                _action(vkCode);
+            }
+
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+    }
+}
