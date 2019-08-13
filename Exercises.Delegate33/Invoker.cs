33e5af265e3c361f30e52973f15d2864d08b08e9
Yauheni.Hlinski <yauheni.hlinski@itechart-group.com>
2019-08-13 17:43:35 +0300

    [33] Delegate - Add invoker

--- Exercises.Delegate33/Invoker.cs
+++ Exercises.Delegate33/Invoker.cs
@@ -1,6 +1,30 @@
-﻿namespace Exercises.Delegate33
+﻿using System;
+using System.Text;
+
+namespace Exercises.Delegate33
 {
     public class Invoker
     {
+        private Func<string> _functions;
+
+        public Invoker(Func<string> functions)
+        {
+            _functions = functions;
+        }
+
+
+        public string GetResultFromFunctions()
+        {
+            var resultStringBuilder = new StringBuilder();
+            var invocationList = _functions.GetInvocationList();
+
+            foreach (var function in invocationList)
+            {
+                var currentResult= function.DynamicInvoke();
+                resultStringBuilder.Append(currentResult);
+            }
+
+            return resultStringBuilder.ToString();
+        }
     }
 }
--- Exercises.Delegate33/Program.cs
+++ Exercises.Delegate33/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace Exercises.Delegate33
+﻿using System;
+
+namespace Exercises.Delegate33
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Func<string> functions = null;
+            functions += () => "1";
+            functions += () => "2";
+            functions += () => "3";
+            var invoker = new Invoker(functions);
+            var result = invoker.GetResultFromFunctions();
+            Console.WriteLine(result);
+            Console.ReadLine();
         }
     }
 }
