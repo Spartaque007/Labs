ad16374033b76e953aae7f642b8432573b971bbc
Yauheni.Hlinski <yauheni.hlinski@itechart-group.com>
2019-08-14 18:02:38 +0300

    [38] Linq - All tasks completed

--- Exercises.Linq/Program.cs
+++ Exercises.Linq/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
 
 namespace Exercises.Linq
 {
@@ -6,20 +8,57 @@ namespace Exercises.Linq
     {
         static void Main(string[] args)
         {
+            TaskData data = new TaskData();
+
             //1) Find the words in the collection that start with the letter 'L' from fruits
+            PrintHead(data.Fruits, 1);
+            var wordsWithL = data.Fruits.Where(f => f[0] == 'L').ToList();
+            PrintResult(wordsWithL);
             //2) Which of the following numbers are multiples of 4 or 6 from mixedNumbers
+            var numbers = data.MixedNumbers.Where(n => n % 4 == 0 || n % 6 == 0).ToList();
             //3) sort list of names in both directions from names
+            var sortedNames = data.Names.OrderBy(n => n);
+            var sortedNamesDesc = data.Names.OrderByDescending(n => n);
             //4)How much money have we made? purchases
+            var summ = data.Purchases.Sum();
             /*5)
                Store each number in the following List until a perfect square
                is detected.
                Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
                wheresSquaredo
             */
+            var h = data.wheresSquaredo.TakeWhile(n => Math.Sqrt(n) % 1 != 0).ToList();
             /*6)
             Given the same customer set, display how many millionaires per bank.
             from customers
             */
+            var millionaires = data.Customers
+                .Where(c => c.Balance >= 1_000_000)
+                .GroupBy(b => b.Bank)
+                .Select(a => new { Bank = a.Key, Count = a.Count() }).ToList();
+
+        }
+
+        static void PrintHead(IEnumerable dataList, int taskNumber)
+        {
+            Console.WriteLine($"******************Task {taskNumber}******************");
+            Console.WriteLine("\n******************Input Data******************\n");
+
+            foreach (var item in dataList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void PrintResult(IEnumerable resultList)
+        {
+            Console.WriteLine("\nResults:\n");
+
+            foreach (var item in resultList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("******************End******************\n");
         }
     }
 }
--- Exercises.Linq/TaskData.cs
+++ Exercises.Linq/TaskData.cs
@@ -4,15 +4,14 @@ namespace Exercises.Linq
 {
     public class TaskData
     {
-
-        public List<string> fruits = new List<string>()
+        public List<string> Fruits { get; } = new List<string>()
             {
                 "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry"
             };
 
-        public List<int> mixedNumbers = new List<int>() { 15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96 };
+        public List<int> MixedNumbers { get; } = new List<int>() { 15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96 };
 
-        public List<string> names = new List<string>()
+        public List<string> Names { get; } = new List<string>()
             {
                 "Heather", "James", "Xavier", "Michelle", "Brian", "Nina",
                 "Kathleen", "Sophia", "Amir", "Douglas", "Zarley", "Beatrice",
@@ -21,7 +20,7 @@ namespace Exercises.Linq
                 "Francisco", "Tre"
             };
 
-        public List<double> purchases = new List<double>()
+        public List<double> Purchases { get; } = new List<double>()
             {
                 2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
             };
@@ -31,17 +30,17 @@ namespace Exercises.Linq
                 66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
             };
 
-        public List<Customer> customers = new List<Customer>() {
-        new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
-        new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
-        new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
-        new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
-        new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
-        new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
-        new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
-        new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
-        new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
-        new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+        public List<Customer> Customers { get; } = new List<Customer>() {
+        new Customer(){ Name="Bob Lesman", Balance=80_345.66, Bank="FTB"}, 
+        new Customer(){ Name="Joe Landy", Balance=9_284_756.21, Bank="WF"},//WF"
+        new Customer(){ Name="Meg Ford", Balance=487_233.01, Bank="BOA"},
+        new Customer(){ Name="Peg Vale", Balance=7_001_449.92, Bank="BOA"},//BOA
+        new Customer(){ Name="Mike Johnson", Balance=790_872.12, Bank="WF"},
+        new Customer(){ Name="Les Paul", Balance=8_374_892.54, Bank="WF"},//WF
+        new Customer(){ Name="Sid Crosby", Balance=957_436.39, Bank="FTB"},
+        new Customer(){ Name="Sarah Ng", Balance=56_562_389.85, Bank="FTB"},//FTB
+        new Customer(){ Name="Tina Fey", Balance=1_000_000.00, Bank="CITI"},//
+        new Customer(){ Name="Sid Brown", Balance=49_582.68, Bank="CITI"}
         };
     }
 
@@ -59,5 +58,4 @@ namespace Exercises.Linq
         public double Balance { get; set; }
         public string Bank { get; set; }
     }
-
 }
