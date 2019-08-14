e01999a2de83524b0529fe419e6c36d4a44749b9
Yauheni.Hlinski <yauheni.hlinski@itechart-group.com>
2019-08-14 14:58:54 +0300

    [38] Linq - TaskData add

--- Exercises.Linq/Program.cs
+++ Exercises.Linq/Program.cs
@@ -1,10 +1,25 @@
-﻿
+﻿using System;
+
 namespace Exercises.Linq
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //1) Find the words in the collection that start with the letter 'L' from fruits
+            //2) Which of the following numbers are multiples of 4 or 6 from mixedNumbers
+            //3) sort list of names in both directions from names
+            //4)How much money have we made? purchases
+            /*5)
+               Store each number in the following List until a perfect square
+               is detected.
+               Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
+               wheresSquaredo
+            */
+            /*6)
+            Given the same customer set, display how many millionaires per bank.
+            from customers
+            */
         }
     }
 }
