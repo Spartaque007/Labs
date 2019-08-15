1b24d772b806a9c7a7ad7cda73b18c8ff2d02538
Yauheni.Hlinski <yauheni.hlinski@itechart-group.com>
2019-08-15 18:29:47 +0300

    [40] Linq2 - task1 and task2 done

---
+++ Exrercises.Linq2/Exercise.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Exrercises.Linq2
+{
+    class Exercise
+    {
+        // The following method should return true if each element in the squares sequence
+        // is equal to the square of the corresponding element in the numbers sequence.
+        public static bool TestForSquares(IEnumerable<int> numbers, IEnumerable<int> squares)
+        {
+            var a = numbers.Select((number, index) =>
+            {
+                var item = squares.Skip(index).Take(1).Single();
+                return number * number == item;
+            })
+               .All(x => x);
+
+            return a;
+        }
+
+        // Given a sequence of words, get rid of any that don't have the character 'e' in them,
+        // then sort the remaining words alphabetically, then return the following phrase using
+        // only the final word in the resulting sequence:
+        //    -> "The last word is <word>"
+        // If there are no words with the character 'e' in them, then return null.
+        //   
+        // TRY to do it all using only LINQ statements. No loops or if statements.
+        public static string GetTheLastWord(IEnumerable<string> words)
+        {
+            return words.Where(w => !w.Contains('e')).Select(w => w).OrderByDescending(w => w).FirstOrDefault();
+        }
+
+        // There is an array with some numbers.All numbers are equal except for one.Try to find it!
+        //It’s guaranteed that array contains more than 3 numbers.
+        public static T FindUniq<T>(IEnumerable<T> numbers)
+        {
+            var g = numbers.GroupBy(n =>n).Select(n=>new { n.Key, n.Count()}
+                );
+            return numbers.First();
+        }
+    }
+}
--- Exrercises.Linq2/Program.cs
+++ Exrercises.Linq2/Program.cs
@@ -6,12 +6,27 @@ namespace Exrercises.Linq2
     {
         static void Main(string[] args)
         {
-
-            
             var numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            var numdersSqr = new int[] { 1, 4, 9, 16, 25, 36, 49, 64, 81, 100 };
+            var numdersSqr = new int[] { 1, 4, 9, 16, 25, 36, 49, 64, 100, 81 };
+            var result1 = Exercise.TestForSquares(numbers, numdersSqr);
+            Console.WriteLine(result1);
+
+            var words = new string[]
+            {
+                "One", "Two", "Tree ", "four", "five","six"
+            };
+
+            var result2 = Exercise.GetTheLastWord(words);
+            Console.WriteLine(result2);
+
+            var numbers1 =new int[]{1, 1, 1, 2, 1, 1};
+            var numbers2 = new double[] { 0, 0, 0.55, 0, 0 };
+            Console.WriteLine(Exercise.FindUniq(numbers1));
+            Console.WriteLine(Exercise.FindUniq(numbers2));
             
             Console.ReadKey();
         }
+
+
     }
 }
